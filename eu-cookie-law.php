75270996b309041741459d2d06ffc9688900df07
pleer <pleer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-03 09:02:25 +0000

    eu cookie law 1.2 commit
    git-svn-id: https://plugins.svn.wordpress.org/eu-cookie-law/tags/1.2@593917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ eu-cookie-law.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name:  EU Cookie Law
+Plugin URI:   http://3doordigital.com/wordpress/plugins/eu-cookie-law/
+Description:  Inserts a bar within your website to warn users about the EU cookie law, with a popup for more information. Clicking accept will remove the bar for a selected amount of time
+Version:      1.2
+Author:       Shane Jones & Alex Moss
+Author URI:   http://3doordigital.com/
+Contributors: alexmoss, pleer, ShaneJones
+
+Copyright (C) 2010-2012, Shane Jones & Alex Moss
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of Alex Moss or pleer nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+if ( is_admin() && ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) ){
+	require 'class-admin.php';
+	register_activation_hook(__FILE__, 'pea_cook_defaults');
+} else {
+	require 'class-frontend.php';
+}
+
+
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Name ===
+Contributors: alexmoss, pleer, ShaneJones
+Plugin Name:  EU Cookie Law
+Plugin URI:   http://3doordigital.com/wordpress/plugins/eu-cookie-law/
+Donate link:  http://3doordigital.com/go/eu-cookie-paypal/
+Description:  Inserts a bar within your website to warn users about the EU cookie law, with a popup for more information. Clicking accept will remove the bar for a selected amount of time.
+Version:      1.2
+Author:       Alex Moss and Shane Jones
+Author URI:   http://3doordigital.com/
+Link: http://3doordigital.com/wordpress/plugins/facebook-comments/
+Tags: eu cookie, cookies, law, analytics
+Requires at least: 2.7
+Tested up to: 3.4.1
+Stable tag: 1.2
+
+== Description ==
+
+Inserts a bar within your website to warn users about the EU cookie law, with a popup for more information. Clicking accept will remove the bar for a selected amount of time
+
+Simply install the plugin and follow the instructions on the Settings page.
+
+* [EU Cookie Law](http://3doordigital.com/wordpress/plugins/eu-cookie-law/) Plugin homepage.
+* More [WordPress Plugins](http://3doordigital.com/wordpress/plugins/).
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `eu-cookie-law` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the EU Cookie settings page
+4. Go through the steps and hit update!
+
+
+== Changelog ==
+
+= 1.2 =
+
+* Fixed cookie storing bug in Firefox
+
+
+= 1.1 =
+
+* Fixed cookie storing bug
+* Added in CSS support for IE
